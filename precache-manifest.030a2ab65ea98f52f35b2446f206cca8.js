78dfeed6d39a859222ba6d92e43256ae3df322a1
aguayo99 <aguayojuan20@gmail.com>
2023-12-21 23:01:17 +0100

    Updates

---
+++ precache-manifest.030a2ab65ea98f52f35b2446f206cca8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce433f18bfadc7d0ef13843f1ae3166",
+    "url": "/galeyapp/index.html"
+  },
+  {
+    "revision": "cea426d522ac71c3a9cf",
+    "url": "/galeyapp/static/css/main.de331f17.chunk.css"
+  },
+  {
+    "revision": "75900b040b9c131af780",
+    "url": "/galeyapp/static/js/2.6986dc58.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/galeyapp/static/js/2.6986dc58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cea426d522ac71c3a9cf",
+    "url": "/galeyapp/static/js/main.1d05b811.chunk.js"
+  },
+  {
+    "revision": "adc06647e0ba018a00a1",
+    "url": "/galeyapp/static/js/runtime-main.1acd3b8a.js"
+  }
+]);
